2f570b66c9db3e7674ce2d676bde169b27d87671
abhijeeeth <112998926+abhijeeeth@users.noreply.github.com>
2023-11-30 16:20:13 +0530

    Completed YumEats frontend

---
+++ src/Components/SearchModal.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SearchModal() {
+  return (
+    <div className='bg-black/50 max-w-[1520] mx-auto flex justify-between items-center p-4'>
+      
+    </div>
+  )
+}
+
+export default SearchModal
